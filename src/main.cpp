8834ed56c7464d6d6a9fc4d128d969cd4efcceb6
Archie Jaskowicz <archiejaskowicz@gmail.com>
2023-12-18 13:02:25 +0000

    more debug

--- src/main.cpp
+++ src/main.cpp
@@ -217,6 +217,7 @@ void FDR::read_console() {
 	std::vector<std::string> strings;
 	std::string str;
 	while (std::getline(console_file, str)) {
+		std::cout << "str: " << str << "\n";
 		strings.emplace_back(std::move(str));
 	}
 	last_char_read = console_file.tellg();
